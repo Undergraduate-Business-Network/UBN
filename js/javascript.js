db44e0f78e1a75fce02f4c116e848418efffb147
dakong <dakong94@gmail.com>
2015-09-10 16:28:41 -0700

    Updated Member Page
    Changed the darkness of the images
    Increase FadeIn time on landing pages
    Added C Name File

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
 
     
 $(document).ready(function(){
-    $('#home-panel, #first-about-panel, #member-panel, .recruit-banner, #contact-panel').animate({opacity:1}, "750")
+    $('.recruit-banner').animate({opacity: 1}, "slow");
+    $('#home-panel, #first-about-panel, #member-panel').animate({opacity:.8},"slow");
+    $('#contact-panel').animate({opacity:.5},"slow");
     
     $('.sponsor-logo').slick({
       dots: false,
