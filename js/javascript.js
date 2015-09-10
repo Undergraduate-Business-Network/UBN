293f7121f5eb97fe7b7e99830706c65c72b0a45f
dakong <dakong94@gmail.com>
2015-09-09 18:41:02 -0700

    Fixed
    Turns out the bug was my chrome browser……..

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,40 @@
 
     
 $(document).ready(function(){
+    $('#home-panel, #first-about-panel, #member-panel, .recruit-banner, #contact-panel').animate({opacity:1}, "750")
     
-$('.sponsor-logo').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-    
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
+    $('.sponsor-logo').slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 });
 
 
@@ -76,10 +74,7 @@ $(document).scroll(function(){
         $('.navbar').css('-webkit-transition', '.5s');
         $('.navba').css('-ms-transition', '.5s');
 
-
     }
-    
-    
 });
 
 /**
