5d932d6bbf9cc17bfb4f4d5debf17693c2b2c098
dakong <dakong94@gmail.com>
2015-09-09 18:01:44 -0700

    cleaned js

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,5 @@
-//smooth scrolling - JQuery Easin plugin
-$(function() {
-    $('.smooth-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top-50
-        }, 1200, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
 
- 
     
-
-
 $(document).ready(function(){
     
 $('.sponsor-logo').slick({
@@ -51,56 +38,6 @@ $('.sponsor-logo').slick({
   ]
 });
 });
-
-
-						
-/*
-$(document).ready(function(){
-
-  var $window = $(window); 
-  $('section[data-type="background"]').each(function(){
-    var $bgobj = $(this); // assigning the object
-    $(window).scroll(function() {
-     var yPos = -( ($window.scrollTop() - $bgobj.offset().top) / $bgobj.data('speed'));
-      // Put together our final background position
-      var coords = '50% '+ yPos + 'px';
-      // Move the background
-      $bgobj.css({ backgroundPosition: coords });
-      });
-  });
-});
-*/
-
-
-
-//WORK IN PROGRESS -- CLICK TO CHANGE NAVBAR 
-/*var toggle = true;
-
-$('.navbar-toggle').click(function(){
- 
-if($(this).scrollTop() < 125){
-if(toggle == true){
-    $('.navbar').css({"background":"#192f42"});
-    
-    $('.navbar').css({"border-bottom": "4px solid beige"});
-    $('#logo').css({"height": "50px"});
-    $('.navbar').css({"padding-bottom": "0px"});
-    toggle = false;
-  }
-
-else{
-
-  $('.navbar').css({"background":"transparent"});
-       $('.navbar-collapse').css({"background":"transparent"});
-       $('.navbar').css({"border-bottom": "0px solid white"});
-       $('#logo').css({"height": "90px"});
-       toggle = true;
-}
-
-}
-
-});*/
-
 
 
 /**
@@ -188,78 +125,7 @@ $(window).scroll(function(){
         $('#challenge').css('-ms-transition', '.5s');
       
     }
-    
-    
-    
-    /*if($(window).scrollTop() < 10){
-       /* $('#conquer').css('transition', '.5s');
-        $('#conquer').css('-o-transition', '.5s');
-        $('#conquer').css('-moz-transition', '.5s');
-        $('#conquer').css('-webkit-transition', '.5s');
-        $('#conquer').css('-ms-transition', '.5s');
-        $('#conquer').css('opacity','1.0');
-        $('#challenge').css('opacity','0');
-    }
-    else if($(window).scrollTop() > 10
-                && $(window).scrollTop() < 20){
-        $('#conquer').css('opacity','.9');
-        $('#challenge').css('opacity','.1');
-        
-    }
-    else if($(window).scrollTop() > 20
-                && $(window).scrollTop() < 30){
-        $('#conquer').css('opacity','.8');
-        $('#challenge').css('opacity','.2');   
-    }
-    else if($(window).scrollTop() > 30
-                && $(window).scrollTop() < 40){
-        $('#conquer').css('opacity','.7');
-        $('#challenge').css('opacity','.3');   
-    }
-    else if($(window).scrollTop() > 40
-                && $(window).scrollTop() < 55){
-        $('#conquer').css('opacity','.6');
-        $('#challenge').css('opacity','.4');   
-    }
-    else if($(window).scrollTop() > 55
-                && $(window).scrollTop() < 70){
-        $('#conquer').css('opacity','.5');
-        $('#challenge').css('opacity','.5');   
-    }
-    else if($(window).scrollTop() > 70
-                && $(window).scrollTop() < 85){
-        $('#conquer').css('opacity','.4');
-        $('#challenge').css('opacity','.6');   
-    }
-    else if($(window).scrollTop() > 85
-                && $(window).scrollTop() < 100){
-        $('#conquer').css('opacity','.3');
-        $('#challenge').css('opacity','.7');   
-    }
-    else if($(window).scrollTop() > 100
-                && $(window).scrollTop() < 110){
-        $('#conquer').css('opacity','.2');
-        $('#challenge').css('opacity','.8');   
-    }
-    else if($(window).scrollTop() > 110
-                && $(window).scrollTop() < 120){
-        $('#conquer').css('opacity','.1');
-        $('#challenge').css('opacity','.9');   
-    }
-    else if($(window).scrollTop() > 120){
-        $('#conquer').css('opacity','0');
-        $('#challenge').css('opacity','1.0');
-        $('#challenge').css('transition', '.5s');
-        $('#challenge').css('-o-transition', '.5s');
-        $('#challenge').css('-moz-transition', '.5s');
-        $('#challenge').css('-webkit-transition', '.5s');
-        $('#challenge').css('-ms-transition', '.5s');
-      
-    }*/
-    
-    
-    
-    
+  
 });
 
 /**
@@ -355,9 +221,3 @@ function initialize(){
 }
     google.maps.event.addDomListener(window, 'load', initialize);
 
-
-/**TESTING FUNCTIONS **/
-$('.mask').click(function(e){
-    console.log("CLICKED");
-    
-});
